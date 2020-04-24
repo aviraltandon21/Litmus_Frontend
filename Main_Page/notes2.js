ea7568bbfcbf5ff973444f44319b8a39a2485ee8
nlok5923 <56120084+nlok5923@users.noreply.github.com>
2020-04-25 05:29:32 +0530

    minutes-defects-corrected  scroll-button-added

--- Main_Page/notes2.js
+++ Main_Page/notes2.js
@@ -51,3 +51,25 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+var mybutton = document.getElementById("myBtnscroll");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {
+     scrollFunction()
+    };
+ 
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+ 
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0; // this is for mac users
+   document.documentElement.scrollTop = 0; // this is for windows user
+
+ }
